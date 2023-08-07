20f65b9462c859782b994d61c317a558802b0e7b
Shaja Nasir <Shajanasir@gmail.com>
2023-08-07 22:46:05 +0530

    add Main Navigation with Routes

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import classes from "./MainNavigation.module.css";
+import { Link } from "react-router-dom";
+import FavoriteContext from "../../store/favorites-context";
+const MainNavigation = (props) => {
+  const favoriteContext = useContext(FavoriteContext);
+  return (
+    <div className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>All Meetups</Link>
+          </li>
+          <li>
+            <Link to='/new-meetup'>Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to='/favorites'>
+              My Favorite
+              <span className={classes.badge}>
+                {favoriteContext.totalFavorites}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MainNavigation;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { FavoriteContextProvider } from "./store/favorites-context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <FavoriteContextProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </FavoriteContextProvider>
 );
